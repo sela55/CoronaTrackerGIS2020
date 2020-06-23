bc11f8691a74a1e043c4864d77f60bc3fef33c87
Sela Shabtai <sela.shabtai@icloud.com>
2020-06-23 16:05:01 +0300

    my lastest branh

--- CoronaTracker/CoronaTracker/Notifications.xaml.cs
+++ CoronaTracker/CoronaTracker/Notifications.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using RestSharp;
 using Xamarin.Forms;
@@ -13,6 +14,9 @@ namespace CoronaTracker
         {
             InitializeComponent();
 
+            //List<Countries> countries = new List<Countries>();
+
+            string strDebugOutput = null;
             var client = new RestClient("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php");
             var request = new RestRequest(Method.GET);
             //request.RootElement = "country_name";
@@ -22,6 +26,22 @@ namespace CoronaTracker
 
             string restResponse = response.Content;
             var jsonObject = JObject.Parse(response.Content);
+            
+            //string check1 = jsonObject.GetValue();
+
+            deserialiseJSON(restResponse);
+            var Jcountries = JsonConvert.DeserializeObject<dynamic>(restResponse);
+            //string selectedCountry = null;
+   
+
+            //now we are taking to countries list into a list to display it in the picker
+            List<string> countriesList = new List<string>();
+            foreach (var state in Jcountries.countries_stat)
+            {
+                countriesList.Add(state.country_name.ToString());
+            }
+            //string temproryVar = Jcountries.countries_stat.country_name;
+
 
 
 
@@ -33,22 +53,189 @@ namespace CoronaTracker
                 HorizontalOptions = LayoutOptions.Center,
                 VerticalOptions = LayoutOptions.CenterAndExpand,
                 Title = "Search for a Country",
+                TitleColor = Color.Red,
+                ItemsSource = countriesList,
+                //SelectedItem = selectedCountry,
+                
+
                 
 
             };
 
+            Label totalConfirmedLabel = new Label()
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                FontSize = Device.GetNamedSize(NamedSize.Subtitle, typeof(Label)),
+                FontAttributes = FontAttributes.None,
+                Text = "Total Confirmed",
+                TextColor = Color.DarkGray
+            };
+
+            Label numOfTotalConfirmedLabel = new Label()
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                FontAttributes = FontAttributes.Bold,
+                Text = "Total Confirmed Number",
+                TextColor = Color.DarkRed,
+                IsVisible = false
+       
+            };
+
+            Label totalConfirmedDeathsLabel = new Label()
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                FontSize = Device.GetNamedSize(NamedSize.Subtitle, typeof(Label)),
+                FontAttributes = FontAttributes.None,
+                Text = "Total deaths",
+                TextColor = Color.DarkGray
+            };
+
+            Label numOfTotalConfirmedDeathsLabel = new Label()
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                FontAttributes = FontAttributes.Bold,
+                Text = "Total Confirmed",
+                TextColor = Color.Black,
+                IsVisible = false
+            };
+
+
+
             Content = new StackLayout
             {
                 Children = {
-                    new Label {
+                        new Label {
                         Text = "here we will bulid the picker and also search label",
                         HorizontalOptions = LayoutOptions.Center,
                         VerticalOptions = LayoutOptions.CenterAndExpand
+                        
 
 
-                    }, countryPicker
+                    }, countryPicker,
+                        totalConfirmedLabel,
+                        numOfTotalConfirmedLabel,
+                        totalConfirmedDeathsLabel,
+                        numOfTotalConfirmedDeathsLabel
                 }
             };
+
+            void DebugOutput(string strDebugText)
+            {
+                try
+                {
+                    System.Diagnostics.Debug.Write(strDebugText + Environment.NewLine);
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.Write(ex.Message.ToString() + Environment.NewLine);
+                }
+            }
+
+            void deserialiseJSON(string strJSON)
+            {
+                try
+                {
+                    var icountries = JsonConvert.DeserializeObject<Comments>(strJSON);
+                    DebugOutput("heres our JSON object : " + icountries.ToString());
+                    System.Console.WriteLine("JIMJIMJIMJIMJIMJIM" + icountries.countries_stat.country_name);
+                  
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            countryPicker.SelectedIndexChanged += (sender, args) =>
+            {
+                //System.Console.WriteLine("Hello:");
+                if (countryPicker.SelectedIndex == -1)
+                {
+
+                }
+                else
+                {
+                    string countryString = countryPicker.Items[countryPicker.SelectedIndex].ToString();
+                    CheckingStatusaboutSpecificCountry(countryString); 
+                }
+            };
+
+
+            void CheckingStatusaboutSpecificCountry(string countryName)
+            {
+                if (countryName != null)
+                {
+                    var client1 = new RestClient("https://coronavirus-monitor.p.rapidapi.com/coronavirus/latest_stat_by_country.php?country=" + countryName);
+                    var request1 = new RestRequest(Method.GET);
+                    request1.AddHeader("x-rapidapi-host", "coronavirus-monitor.p.rapidapi.com");
+                    request1.AddHeader("x-rapidapi-key", "1e9bfb544dmsh018403b9672dd9ap1b7dfajsncc70eed878e3");
+                    IRestResponse response1 = client1.Execute(request1);
+
+                    string restResponse1 = response1.Content;
+                    var jsonObject1 = JObject.Parse(response1.Content);
+
+                    deserialiseJSON(restResponse1);
+                    var Jcountries1 = JsonConvert.DeserializeObject<dynamic>(restResponse1);
+
+
+                    TurnOnTheLabels(countryName);
+
+                }
+            }
+
+            void TurnOnTheLabels(string countryName)
+            {
+                string numOfSick = jsonObject.GetValue("total_cases").ToString();
+                numOfTotalConfirmedLabel.Text = numOfSick;
+                numOfTotalConfirmedLabel.IsVisible = true;
+
+                string numOfdeaths = jsonObject.GetValue("total_deaths").ToString();
+                numOfTotalConfirmedDeathsLabel.Text = numOfdeaths;
+                numOfTotalConfirmedDeathsLabel.IsVisible = true;
+
+
+            }
+            }
+
+        
+
+        //void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    var picker = (Picker)sender;
+        //    int selectedIndex = picker.SelectedIndex;
+
+        //    if (selectedIndex != -1)
+        //    {
+        //        monkeyNameLabel.Text = (string)picker.ItemsSource[selectedIndex];
+        //    }
+        //}
+    }
+        
+
+        
+
+    
+
+    
+
+    public class Comments
+    {
+        public Countries_Stat countries_stat { get; set; }
+        public class Countries_Stat
+        {
+
+            public string country_name { get; set; }
+            public int cases { get; set; }
+            public int deaths { get; set; }
+            public int total_recovered { get; set; }
         }
     }
+
 }
+
